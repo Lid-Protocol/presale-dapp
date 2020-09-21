f25895f7297c7b76dd27dbdcabb3b33113504a70
Jikun Liao <liaojikundever@gmail.com>
2020-09-21 06:15:46 +0300

    Fix UI bugs, Auto Update UI regarding provider events

--- src/components/DepositForm.tsx
+++ src/components/DepositForm.tsx
@@ -67,7 +67,13 @@ const DepositForm: React.FC<IDepositForm> = ({
     } else if (Number(event.target.value) < 0) {
       setDisplayVal('0');
     } else {
-      setDisplayVal(event.target.value);
+      const inputStr = event.target.value;
+      setDisplayVal(
+        inputStr.indexOf('.') >= 0
+          ? inputStr.substr(0, inputStr.indexOf('.')) +
+              inputStr.substr(inputStr.indexOf('.'), 19)
+          : inputStr
+      );
     }
   };
 
@@ -128,7 +134,8 @@ const DepositForm: React.FC<IDepositForm> = ({
           {`Deposit ETH for ${meta.tokenName}`}
         </Text>
         <Text fontSize="18px" color="blue.500">
-          Minimum 0.01 ETH, Maximum {removeDecimal(fromWei(hardcap))} ETH
+          Minimum 0.01 ETH, Maximum {removeDecimal(fromWei(meta.accountCap))}{' '}
+          ETH
         </Text>
         <Text fontSize="18px" color="red.500">
           Your Available Max: {removeDecimal(fromWei(availableMax))} ETH
