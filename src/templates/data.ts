4a115c4887f1b648f7867dc271bc01443c31540c
Jikun Liao <liaojikundever@gmail.com>
2020-09-23 19:52:13 -0500

    Fix template names and urls

--- src/templates/data.ts
+++ src/templates/data.ts
@@ -2,7 +2,7 @@ export const data = [
   {
     projectUrl: 'https://dextrust.lid.sh',
     projectName: 'Dex Trust',
-    projectImage: 'https://swapfolio.lid.sh/opengraph.jpg'
+    projectImage: 'https://dextrust.lid.sh/opengraph.jpg'
   },
   {
     projectUrl: 'https://finswap.lid.sh',
@@ -19,6 +19,11 @@ export const data = [
     projectName: 'XFI',
     projectImage: 'https://xfi.lid.sh/opengraph.jpg'
   },
+  {
+    projectUrl: 'https://swapfolio.lid.sh',
+    projectName: 'SwapFolio',
+    projectImage: 'https://swapfolio.lid.sh/opengraph.jpg'
+  }
   // {
   //   projectUrl: 'https://myx.lid.sh',
   //   projectName: 'MyX',
