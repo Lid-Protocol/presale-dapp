cc8b0b68bf2d2550aa933c897fa0bf9af4625cab
Jikun Liao <liaojikundever@gmail.com>
2020-09-28 19:08:34 -0500

    Revert claimPeriod change

--- src/components/Claimer.tsx
+++ src/components/Claimer.tsx
@@ -33,7 +33,7 @@ const Claimer: React.FC<IClaimer> = ({
   isRefunding
 }) => {
   const [active, setActive] = useState(true);
-  const [accountAddress, setAccountAddress] = useState("placeHolder");
+  const [accountAddress, setAccountAddress] = useState('placeHolder');
 
   const redeemPercent = Math.floor(
     Number(redeemBP) / 100 / (Number(redeemInterval) / 3600)
@@ -41,23 +41,25 @@ const Claimer: React.FC<IClaimer> = ({
 
   const updateAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAccountAddress(event.target.value);
-  }
+  };
 
   const handleSubmit = async function () {
     if (!lidPresaleSC) {
       return;
     }
 
-    await lidPresaleSC.methods.getRefundableEth(accountAddress).call(function (error: any, result: any){
-      if (result <= 0) {
-        alert("Refundable Eth is less than 0");
-        return;
-      }
-    })
+    await lidPresaleSC.methods
+      .getRefundableEth(accountAddress)
+      .call(function (error: any, result: any) {
+        if (result <= 0) {
+          alert('Refundable Eth is less than 0');
+          return;
+        }
+      });
 
     await lidPresaleSC.methods
       .claimRefund(accountAddress)
-      .send({from: accountAddress });
+      .send({ from: accountAddress });
     alert(
       'Deposit request sent. Check your wallet to see when it has completed, then refresh this page.'
     );
@@ -96,7 +98,68 @@ const Claimer: React.FC<IClaimer> = ({
   if (isRefunding) {
     return (
       <>
-    <Box
+        <Box
+          w="100%"
+          maxWidth="1200px"
+          ml="auto"
+          mr="auto"
+          mt="60px"
+          mb="60px"
+          px={['20px', '20px', 0]}
+        >
+          <Box
+            textAlign="center"
+            border="solid 1px"
+            borderRadius="5px"
+            borderColor="lid.stroke"
+            bg="white"
+            display="block"
+            w="100%"
+            mb="20px"
+            p="20px"
+          >
+            <Text fontSize={['24px', '36px']} fontWeight="bold">
+              {`Claim Refund`}
+            </Text>
+
+            <Input
+              fontSize="18px"
+              w="100%"
+              maxW="600px"
+              mb="0px"
+              display="inline-block"
+              min={0.01}
+              mt="10px"
+              onChange={updateAddress}
+            />
+
+            <Button
+              isDisabled={accountRedeemable === '0'}
+              variantColor="blue"
+              bg="lid.brand"
+              color="white"
+              border="none"
+              display="block"
+              borderRadius="25px"
+              w="200px"
+              h="50px"
+              m="0px"
+              mt="30px"
+              fontWeight="regular"
+              fontSize="18px"
+              ml="auto"
+              mr="auto"
+              onClick={handleSubmit}
+            >
+              Send
+            </Button>
+          </Box>
+        </Box>
+      </>
+    );
+  } else {
+    return (
+      <Box
         w="100%"
         maxWidth="1200px"
         ml="auto"
@@ -117,20 +180,14 @@ const Claimer: React.FC<IClaimer> = ({
           p="20px"
         >
           <Text fontSize={['24px', '36px']} fontWeight="bold">
-            {`Claim Refund`}
+            {`Claim Your ${meta.tokenSymbol}`}
           </Text>
-    
-          <Input
-            fontSize="18px"
-            w="100%"
-            maxW="600px"
-            mb="0px"
-            display="inline-block"
-            min={0.01}
-            mt="10px"
-            onChange={updateAddress}
-          />
-
+          <Text fontSize="18px" color="blue.500">
+            {redeemPercent}% released / hour
+          </Text>
+          <Text fontSize="18px" color="lid.fg">
+            {`${meta.tokenSymbol} to Claim: ${shortEther(accountRedeemable)}`}
+          </Text>
           <Button
             isDisabled={accountRedeemable === '0'}
             variantColor="blue"
@@ -147,141 +204,85 @@ const Claimer: React.FC<IClaimer> = ({
             fontSize="18px"
             ml="auto"
             mr="auto"
-            onClick={handleSubmit}
+            onClick={handleClaim}
           >
-            Send
+            Claim
           </Button>
         </Box>
-        </Box>
-        </>
-        )
-  } else {
-
-  return (
-    <Box
-      w="100%"
-      maxWidth="1200px"
-      ml="auto"
-      mr="auto"
-      mt="60px"
-      mb="60px"
-      px={['20px', '20px', 0]}
-    >
-      <Box
-        textAlign="center"
-        border="solid 1px"
-        borderRadius="5px"
-        borderColor="lid.stroke"
-        bg="white"
-        display="block"
-        w="100%"
-        mb="20px"
-        p="20px"
-      >
-        <Text fontSize={['24px', '36px']} fontWeight="bold">
-          {`Claim Your ${meta.tokenSymbol}`}
-        </Text>
-        <Text fontSize="18px" color="blue.500">
-          {redeemPercent}% released / hour
-        </Text>
-        <Text fontSize="18px" color="lid.fg">
-          {`${meta.tokenSymbol} to Claim: ${shortEther(accountRedeemable)}`}
-        </Text>
-        <Button
-          isDisabled={accountRedeemable === '0'}
-          variantColor="blue"
-          bg="lid.brand"
-          color="white"
-          border="none"
-          display="block"
-          borderRadius="25px"
-          w="200px"
-          h="50px"
-          m="0px"
-          mt="30px"
-          fontWeight="regular"
-          fontSize="18px"
-          ml="auto"
-          mr="auto"
-          onClick={handleClaim}
-        >
-          Claim
-        </Button>
-      </Box>
-      <Grid
-        w="100%"
-        gap="20px"
-        mb="40px"
-        templateRows={['repeat(2, 1fr)', 'max-content']}
-        templateColumns={['auto', 'repeat(2, minmax(0, 1fr))']}
-      >
-        <Box
+        <Grid
           w="100%"
-          borderRadius="5px"
-          p="25px"
-          border="solid 1px"
-          borderColor="lid.stroke"
-          bg="lid.bg"
+          gap="20px"
+          mb="40px"
+          templateRows={['repeat(2, 1fr)', 'max-content']}
+          templateColumns={['auto', 'repeat(2, minmax(0, 1fr))']}
         >
-          <Text fontSize="18px" m="0" p="0" color="lid.fgMed">
-            {`Total ${meta.tokenSymbol} Claimed`}
-          </Text>
-          <Text fontSize="38px" w="100%" fontWeight="bold">
-            {shortEther(accountClaimedTokens)}
-          </Text>
-        </Box>
-        <Box
-          w="100%"
-          borderRadius="5px"
-          p="25px"
-          border="solid 1px"
-          borderColor="lid.stroke"
-          bg="lid.bg"
-        >
-          <Text fontSize="18px" m="0" p="0" color="lid.fgMed">
-            {`${meta.tokenSymbol} / Hour`}
-          </Text>
-          <Text fontSize="38px" w="100%" fontWeight="bold">
-            {maxShares !== '0'
-              ? shortEther(
-                  toBN(accountShares)
-                    .mul(toBN(toWei(meta.totalPresale)))
-                    .div(toBN(maxShares))
-                    .mul(toBN('2'))
-                    .div(toBN('100'))
-                    .toString()
-                )
-              : '0'}
-          </Text>
-        </Box>
-      </Grid>
-      <Box
-        textAlign="center"
-        border="solid 1px"
-        borderRadius="5px"
-        borderColor="lid.stroke"
-        bg="white"
-        display="block"
-        w="100%"
-        mb="20px"
-        p="20px"
-      >
-        {active ? (
-          <>
-            <Text fontSize="18px" color="lid.fg">
-              {`More ${meta.tokenSymbol} available to claim in`}
+          <Box
+            w="100%"
+            borderRadius="5px"
+            p="25px"
+            border="solid 1px"
+            borderColor="lid.stroke"
+            bg="lid.bg"
+          >
+            <Text fontSize="18px" m="0" p="0" color="lid.fgMed">
+              {`Total ${meta.tokenSymbol} Claimed`}
             </Text>
-            <CountDownShort expiryTimestamp={claimPeriod} />
-          </>
-        ) : (
-          <Text fontSize="18px" color="lid.fg">
-            {`All ${meta.tokenSymbol} available to claim`}
-          </Text>
-        )}
+            <Text fontSize="38px" w="100%" fontWeight="bold">
+              {shortEther(accountClaimedTokens)}
+            </Text>
+          </Box>
+          <Box
+            w="100%"
+            borderRadius="5px"
+            p="25px"
+            border="solid 1px"
+            borderColor="lid.stroke"
+            bg="lid.bg"
+          >
+            <Text fontSize="18px" m="0" p="0" color="lid.fgMed">
+              {`${meta.tokenSymbol} / Hour`}
+            </Text>
+            <Text fontSize="38px" w="100%" fontWeight="bold">
+              {maxShares !== '0'
+                ? shortEther(
+                    toBN(accountShares)
+                      .mul(toBN(toWei(meta.totalPresale)))
+                      .div(toBN(maxShares))
+                      .mul(toBN('2'))
+                      .div(toBN('100'))
+                      .toString()
+                  )
+                : '0'}
+            </Text>
+          </Box>
+        </Grid>
+        <Box
+          textAlign="center"
+          border="solid 1px"
+          borderRadius="5px"
+          borderColor="lid.stroke"
+          bg="white"
+          display="block"
+          w="100%"
+          mb="20px"
+          p="20px"
+        >
+          {active ? (
+            <>
+              <Text fontSize="18px" color="lid.fg">
+                {`More ${meta.tokenSymbol} available to claim in`}
+              </Text>
+              <CountDownShort expiryTimestamp={claimPeriod * 1000} />
+            </>
+          ) : (
+            <Text fontSize="18px" color="lid.fg">
+              {`All ${meta.tokenSymbol} available to claim`}
+            </Text>
+          )}
+        </Box>
       </Box>
-    </Box>
-  );
-        }
+    );
+  }
 };
 
 export default Claimer;
