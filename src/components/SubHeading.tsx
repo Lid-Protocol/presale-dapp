d83d1a009ed1e0cd815ee23e2eb6e95ccd796ced
Carlsbad <carlsbad_sunshine@protonmail.com>
2020-10-20 23:07:49 -0700

    Fix countdown timer

--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -45,6 +45,7 @@ const SubHeadings: React.FC<ISubHeading> = ({
   accessTime
 }) => {
   const { addresses } = meta;
+  console.log(expiryTimestamp)
   return (
     <Box
       w="100%"
@@ -286,6 +287,11 @@ const SubHeadings: React.FC<ISubHeading> = ({
             />
 
             {!expiryTimestamp && isActive && (
+              <p>Countdown timer will start once softcap is hit.</p>
+            )
+            }
+
+            {(expiryTimestamp && isActive) && (
               <EndTimer
                 expiryTimestamp={expiryTimestamp}
                 hardcap={hardcap}
